b26532d7c224aee55dfa0297f77d08140aa5384d
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-12-29 16:47:06 +0100

    update

--- config/config.js
+++
@@ -1,4 +0,0 @@
-$.Oda.Context.host = "http://localhost/ODA_OTR/";
-$.Oda.Context.rest = "http://localhost/ODA_OTR/server/";
-$.Oda.Context.resources = "http://localhost/ODA_OTR/server/resources/";
-$.Oda.Context.projectLabel = "OTR";
--- server/config/config.php
+++ server/config/config.php
@@ -1,10 +1,10 @@
 <?php
 $config = \Oda\SimpleObject\OdaConfig::getInstance();
-$config->urlServer = "http://localhost/server/";
+$config->urlServer = "http://localhost/otr/server/";
 $config->resourcesPath = "resources/";
 
 $config->BD_ENGINE->prefixTable = 'otr-';
-$config->BD_ENGINE->base = 'oda_otr';
+$config->BD_ENGINE->base = 'otr';
 $config->BD_ENGINE->mdp = 'hunter';
 $config->BD_ENGINE->host = 'localhost';
-$config->BD_ENGINE->login = 'oda_otr';
+$config->BD_ENGINE->login = 'otr';
